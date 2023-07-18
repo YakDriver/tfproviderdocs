9035f79e1a4ccc55946f9beaedba3ae57d71e839
Dirk Avery <dirk.avery@gmail.com>
2023-07-17 21:28:52 -0400

    Fix error checking

--- check/contents/check_attributes_section.go
+++ check/contents/check_attributes_section.go
@@ -54,7 +54,16 @@ func (d *Document) checkAttributesSection() error {
 	case 1:
 		paragraphText := string(paragraphs[0].Text(d.source))
 
-		if paragraphText != expectedBylineTexts[0] && paragraphText != expectedBylineTexts[1] && paragraphText != expectedBylineTexts[2] && paragraphText != expectedBylineTexts[3] {
+		found := false
+
+		for _, v := range expectedBylineTexts {
+			if paragraphText == v {
+				found = true
+				break
+			}
+		}
+
+		if !found {
 			return fmt.Errorf("attribute section byline (%s) should be: %q, %q, %q, or %q", paragraphText, expectedBylineTexts[0], expectedBylineTexts[1], expectedBylineTexts[2], expectedBylineTexts[3])
 		}
 	}
